d54bcccb40355e2d785c6455be6467baa193f8fa
pedroix <pedroix>
2007-01-29 01:05:48 +0000

    Multiple joining feature

--- smartsearchns.class.php
+++ smartsearchns.class.php
@@ -1,7 +1,8 @@
-<?php /* SMARTSEARCHNS$Id: smartsearchns.class.php,v 1.1 2006/11/02 08:39:30 pedroix Exp $ */
+<?php /* SMARTSEARCHNS$Id: smartsearchns.class.php,v 1.1 2006/11/03 17:08:43 pedroix Exp $ */
 class smartsearchns  {
 
 	var $table = null;
+	var $table_alias = null;
 	var $table_module	= null;
 	var $table_key = null;	// primary key in searched table
 	var $table_key2 = null; // primary key in parent table
@@ -12,6 +13,7 @@ class smartsearchns  {
 	var $table_extra = null;
 	var $search_fields = array ();
 	var $display_fields = array ();
+	var $table_joins = array ();
 	var $keyword = null;
 	var $keywords = null;
 	var $tmppattern = "";
@@ -59,7 +61,7 @@ class smartsearchns  {
 						$ii++;
 						if (!($this->search_options['display_all_flds']=="on") && ($ii>2))
 							break;
-						$display_val=$display_val." ".$records[$fld];
+						$display_val=$display_val." ".$records[preg_replace('/^.*\.([^\.]+)$/','$1',$fld)];
 					}	
 //--MSy-				
 					$tmplink="";
@@ -95,24 +97,36 @@ class smartsearchns  {
 	
 	function _buildQuery(){
                 $q  = new DBQuery;
-                $q->addTable($this->table);
+                
+                if ($this->table_alias) {
+                        $q->addTable($this->table, $this->table_alias);
+                } else {
+                        $q->addTable($this->table);                
+                }
                 $q->addQuery($this->table_key);
-                if (isset($this->table_key2))
-                	$q->addQuery($this->table_key2);
+                if (isset($this->table_key2)) {
+                        $q->addQuery($this->table_key2);
+                }
 //--MSy--
-  				foreach($this->display_fields as $fld){
-					$q->addQuery($fld);
-				}
+                foreach($this->table_joins as $join){
+                  	$q->addJoin($join['table'],$join['alias'],$join['join']);
+                }
+
+                foreach($this->display_fields as $fld){
+                  	$q->addQuery($fld);
+                }
+                
                 $q->addOrder($this->table_orderby);
 				
-                if ($this->table_extra)
-                	$q->addWhere($this->table_extra);
+                if ($this->table_extra) {
+                	      $q->addWhere($this->table_extra);
+   	          }
                 
                 $sql = '';
-				foreach(array_keys($this->keywords) as $keyword) {
+                foreach(array_keys($this->keywords) as $keyword) {
 				$sql.= '(';
 
-					foreach($this->search_fields as $field){
+				foreach($this->search_fields as $field){
 //OR treatment to each keyword
 // Search for semi-colons, commas or spaces and allow any to be separators
 						$or_keywords = preg_split('/[\s,;]+/', $keyword);
@@ -130,13 +144,14 @@ class smartsearchns  {
 								else
 									$sql.=" $field LIKE BINARY '%$or_keyword%' or ";
 						}
-					} // foreach $field
-					$sql = substr($sql,0,-4);
+				} // foreach $field
+				$sql = substr($sql,0,-4);
 
-					if ($this->search_options['all_words']=="on")
+				if ($this->search_options['all_words']=="on") {
 						$sql.= ') and ';
-					else
+				} else {
 						$sql.= ') or ';
+				}
 
                 } // foreach $keyword
 //--MSy--					
@@ -148,5 +163,103 @@ class smartsearchns  {
                 	return '/* */';
                 }
 	}
+}
+
+function highlight($text, $keyval) {
+      global $ssearch;
+      
+      $txt = $text;
+      $hicolor = array("#FFFF66","#ADD8E6","#90EE8A","#FF99FF");
+      $keys=array();
+      if (!is_array($keyval)) 
+      	$keys=array($keyval);
+      else
+      	$keys=$keyval;
+      	
+      foreach ($keys as $key) {
+      	if (strlen($key[0])>0) {
+      	      $key[0] = stripslashes($key[0]);
+      		if (isset ($ssearch['ignore_specchar']) && ($ssearch['ignore_specchar']=="on") ) {
+      			if ($ssearch['ignore_case']=='on')
+            			$txt= eregi_replace ( (recode2regexp_utf8($key[0])), "<span style=\"background:".$hicolor[$key[1]]."\" >\\0</span>", $txt ); 
+      			else
+            			$txt= ereg_replace ( (recode2regexp_utf8($key[0])), "<span style=\"background:".$hicolor[$key[1]]."\" >\\0</span>", $txt ); 
+     			} elseif (!isset($ssearch['ignore_specchar']) || ($ssearch['ignore_specchar']=="") ) {
+      			if ($ssearch['ignore_case']=='on')
+            			$txt= eregi_replace ( $key[0], "<span style=\"background:".$hicolor[$key[1]]."\" >\\0</span>", $txt ); 
+      			else
+            			$txt= ereg_replace ( $key[0], "<span style=\"background:".$hicolor[$key[1]]."\" >\\0</span>", $txt );      			
+     			} else {
+     			    $txt= eregi_replace ( (sql_regcase($key[0])), "<span style=\"background:".$hicolor[$key[1]]."\" >\\0</span>", $txt ); 
+		      }
+      	}
+      }
+      return $txt; 
+}
+
+function recode2regexp_utf8($input) {
+      $result="";
+      for($i=0; $i<strlen($input); ++$i)
+      switch($input[$i]) {
+          case 'A':
+          case 'a':
+      		$result.="(a|A!|A¤|A?|A„)";
+      		break;
+          case 'C':
+          case 'c':
+      		$result.="(c|Ä?|ÄO)";
+      		break;
+          case 'D':
+          case 'd':
+      		$result.="(d|Ä?|ÄŽ)";
+      		break;
+          case 'E':
+          case 'e':
+      		$result.="(e|A©|Ä›|A‰|Äš)";
+      		break;
+          case 'I':
+          case 'i':
+      		$result.="(i|A­|A?)";
+      		break;
+          case 'L':
+          case 'l':
+      		$result.="(l|Äo|Ä3|Ä1|Ä1)";
+      		break;
+          case 'N':
+          case 'n':
+      		$result.="(n|A^|A‡)";
+      		break;
+          case 'O':
+          case 'o':
+      		$result.="(o|A3|A´|A“|A”)";
+      		break;
+          case 'R':
+          case 'r':
+      		$result.="(r|A•|A™|A”|A~)";
+      		break;
+          case 'S':
+          case 's':
+      		$result.="(s|A!|A )";
+      		break;
+          case 'T':
+          case 't':
+      		$result.="(t|AY|A¤)";
+      		break;
+          case 'U':
+          case 'u':
+      		$result.="(u|Ao|A—|Aš|A®)";
+      		break;
+          case 'Y':
+          case 'y':
+      		$result.="(y|A1|A?)";
+      		break;
+          case 'Z':
+          case 'z':
+      		$result.="(z|A3|A1)";
+      		break;
+          default:
+      		$result.=$input[$i];
+      }
+      return $result;
 }	
 ?>
