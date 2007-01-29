4cf2ec24ef1704be8bc7e37987aa4e63f3681b20
pedroix <pedroix>
2007-01-29 01:11:27 +0000

    Private events are now properly filtered

--- searchobjects/events.inc.php
+++ searchobjects/events.inc.php
@@ -1,4 +1,4 @@
-<?php /* SMARTSEARCHNS$Id: events.php,v 1.0 2006/11/02 14:30:00 pedroix Exp $ */
+<?php /* SMARTSEARCHNS$Id: events.inc.php,v 1.1 2006/11/03 17:08:44 pedroix Exp $ */
 /**
 * events Class
 */
@@ -6,14 +6,20 @@ class events extends smartsearchns {
 	var $table = "events";
 	var $table_module	= "calendar";
 	var $table_key = "event_id";
+	var $table_extra = '';
 	var $table_link = "index.php?m=calendar&a=view&event_id=";
 	var $table_title = "Events";
 	var $table_orderby = "event_start_date";
 	var $search_fields = array ("event_title","event_description","event_start_date","event_end_date");
 	var $display_fields = array ("event_title","event_description","event_start_date","event_end_date");
 
+	function events (){
+	      global $AppUI;
+	      $this->table_extra = '(event_private = 0 or event_owner = '.$AppUI->user_id.')';	
+	}
+
 	function cevents (){
-		return new events();
+	      return new events();
 	}
 }
 ?>
